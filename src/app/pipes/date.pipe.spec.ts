d31d798d6b22826fa77a5ae580baaf4087b8f5cf
Nitish Rajput <nitishrajput01@gmail.com>
2020-07-16 19:26:43 +0530

    date pipe spec

--- src/app/pipes/date.pipe.spec.ts
+++ src/app/pipes/date.pipe.spec.ts
@@ -1,7 +1,9 @@
 import { DatePipe } from './date.pipe';
+import * as moment from 'moment';
+
 describe('DateCasePipe', () => {
     let pipe = new DatePipe()
     it('transform "2020-07-11T14:58:48.000Z" to "5 days ago"', () => {
-        expect(pipe.transform('2020-07-11T14:58:48.000Z', [])).toBe('4 days ago');
+        expect(pipe.transform('2020-07-11T14:58:48.000Z', [])).toBe(moment('2020-07-11T14:58:48.000Z').fromNow());
     });
 });
